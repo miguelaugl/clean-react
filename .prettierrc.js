38af7f1b138d5718bfd5bf78d7fd9fa45519e98b
Miguel <miguelaugl@outlook.com>
2021-07-24 14:19:42 -0300

    chore: add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "singleQuote": true,
+  "jsxSingleQuote": true,
+  "quoteProps": "as-needed",
+  "trailingComma": "all",
+  "useTabs": false,
+  "tabWidth": 2,
+  "bracketSpacing": true,
+  "jsxBracketSameLine": false,
+  "arrowParens": "always",
+  "endOfLine": "auto",
+  "printWidth": 100,
+  "semi": true
+}
