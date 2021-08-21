737b23d77096d58727fd558812b2151d1db388bd
Miguel <miguelaugl@outlook.com>
2021-08-21 16:39:49 -0300

    feat: prevent Login to submit if its invalid

--- src/presentation/pages/Login/index.tsx
+++ src/presentation/pages/Login/index.tsx
@@ -23,7 +23,9 @@ export const Login = ({ validation, authentication }: Props) => {
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
     event.preventDefault();
 
-    if (state.isLoading) return;
+    if (state.isLoading || state.emailError || state.passwordError) {
+      return;
+    }
 
     setState({ ...state, isLoading: true });
     await authentication.auth({ email: state.email, password: state.password });
@@ -42,7 +44,7 @@ export const Login = ({ validation, authentication }: Props) => {
       <LoginHeader />
 
       <FormContextProvider value={{ state, setState }}>
-        <form className={styles.form} onSubmit={handleSubmit}>
+        <form data-testid='form' className={styles.form} onSubmit={handleSubmit}>
           <h2>Login</h2>
 
           <Input type='email' name='email' placeholder='Digite seu e-mail' />
