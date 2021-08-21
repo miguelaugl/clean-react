6cc78ec474239f1d833fe19e0292477b9353b764
Miguel <miguelaugl@outlook.com>
2021-08-21 20:29:13 -0300

    feat: ensure EmailValidation returns falsy if email is valid

--- src/validation/validators/email/email-validation.ts
+++ src/validation/validators/email/email-validation.ts
@@ -4,7 +4,9 @@ import { FieldValidation } from '@/validation/protocols/field-validation';
 export class EmailValidation implements FieldValidation {
   constructor(readonly field: string) {}
 
-  validate(): Error {
-    return new InvalidFieldError();
+  validate(value: string): Error {
+    const emailRegex =
+      /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+    return emailRegex.test(value) ? null : new InvalidFieldError();
   }
 }
