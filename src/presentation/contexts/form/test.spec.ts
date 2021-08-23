4b32b74ec86adca8cda4e66fa3eb0054ab2001d1
Miguel <miguelaugl@outlook.com>
2021-08-22 22:36:54 -0300

    test: cleanup afterEach test

--- src/presentation/contexts/form/test.spec.ts
+++ src/presentation/contexts/form/test.spec.ts
@@ -1,7 +1,9 @@
-import { renderHook } from '@testing-library/react-hooks';
+import { renderHook, cleanup } from '@testing-library/react-hooks';
 import { useForm } from '.';
 
 describe('useForm Hook', () => {
+  afterEach(cleanup);
+
   it('should return an error if no context is informed', () => {
     const { result } = renderHook(() => useForm());
     const error = result.current;
