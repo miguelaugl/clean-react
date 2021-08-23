f769423ecc6e4d9af6509d74b9ff49c0e1b4b425
Miguel <miguelaugl@outlook.com>
2021-08-22 22:52:16 -0300

    test: ensure useForm hook return ctx values if ctx is provided

--- src/presentation/contexts/form/test.spec.ts
+++ src/presentation/contexts/form/test.spec.ts
@@ -1,5 +1,6 @@
 import { renderHook, cleanup } from '@testing-library/react-hooks';
-import { useForm } from '.';
+import faker from 'faker';
+import { FormContextProvider, useForm } from '.';
 
 describe('useForm Hook', () => {
   afterEach(cleanup);
@@ -8,5 +9,14 @@ describe('useForm Hook', () => {
     const { result } = renderHook(() => useForm());
     const error = result.current;
     expect(error).toEqual(new Error('FormContext not found.'));
+  });
+
+  it('should return an error if no context is informed', () => {
+    const ctxValue = { email: faker.internet.email(), password: faker.internet.password() };
+    const { result } = renderHook(() => useForm(), {
+      wrapper: FormContextProvider,
+      initialProps: { value: ctxValue },
+    });
+    expect(result.current).toEqual(ctxValue);
   });
 });
