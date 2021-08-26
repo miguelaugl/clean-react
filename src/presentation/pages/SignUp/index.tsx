d777039510dc4292437cc4c540283dbc1ccd844f
Miguel <miguelaugl@outlook.com>
2021-08-26 10:08:45 -0300

    feat: ensure SignUp change passwordConfirmation status to error if validation fails

--- src/presentation/pages/SignUp/index.tsx
+++ src/presentation/pages/SignUp/index.tsx
@@ -16,6 +16,7 @@ export const SignUp = ({ validation }: Props) => {
     name: '',
     email: '',
     password: '',
+    passwordConfirmation: '',
     nameError: '',
     emailError: '',
     passwordError: '',
@@ -29,8 +30,12 @@ export const SignUp = ({ validation }: Props) => {
       nameError: validation.validate('name', state.name),
       emailError: validation.validate('email', state.email),
       passwordError: validation.validate('password', state.password),
+      passwordConfirmationError: validation.validate(
+        'passwordConfirmation',
+        state.passwordConfirmation,
+      ),
     });
-  }, [state.name, state.email, state.password]);
+  }, [state.name, state.email, state.password, state.passwordConfirmation]);
 
   return (
     <div className={styles.signUp}>
