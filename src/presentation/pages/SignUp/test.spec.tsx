8d8a3555d30f4d8b6abb6d596ce9bfd1aac2ccb1
Miguel <miguelaugl@outlook.com>
2021-08-26 10:00:34 -0300

    test: ensure SignUp change name status to error if validation fails

--- src/presentation/pages/SignUp/test.spec.tsx
+++ src/presentation/pages/SignUp/test.spec.tsx
@@ -1,7 +1,8 @@
-import { cleanup, render, RenderResult } from '@testing-library/react';
+import { cleanup, fireEvent, render, RenderResult } from '@testing-library/react';
+import faker from 'faker';
 import React from 'react';
 
-import { FormHelper } from '@/presentation/test';
+import { FormHelper, ValidationStub } from '@/presentation/test';
 
 import { SignUp } from '.';
 
@@ -9,24 +10,42 @@ type SutTypes = {
   sut: RenderResult;
 };
 
-const makeSut = (): SutTypes => {
-  const sut = render(<SignUp />);
+type SutParams = {
+  validationError: string;
+};
+
+const makeSut = (params?: SutParams): SutTypes => {
+  const validationStub = new ValidationStub();
+  validationStub.errorMessage = params?.validationError;
+  const sut = render(<SignUp validation={validationStub} />);
   return {
     sut,
   };
+};
+
+const populateField = (sut: RenderResult, fieldName: string, value = faker.random.word()): void => {
+  const input = sut.getByTestId(fieldName);
+  fireEvent.input(input, { target: { value } });
 };
 
 describe('SignUp Component', () => {
   afterEach(cleanup);
 
   it('should start with initial state', () => {
-    const validationError = 'Campo obrigatório';
-    const { sut } = makeSut();
+    const validationError = faker.random.words();
+    const { sut } = makeSut({ validationError });
     FormHelper.testChildCount(sut, 'error-wrap', 0);
     FormHelper.testButtonIsDisabled(sut, 'submit');
     FormHelper.testStatusForField(sut, 'name', validationError);
-    FormHelper.testStatusForField(sut, 'email', validationError);
-    FormHelper.testStatusForField(sut, 'password', validationError);
-    FormHelper.testStatusForField(sut, 'passwordConfirmation', validationError);
+    FormHelper.testStatusForField(sut, 'email', 'Campo obrigatório');
+    FormHelper.testStatusForField(sut, 'password', 'Campo obrigatório');
+    FormHelper.testStatusForField(sut, 'passwordConfirmation', 'Campo obrigatório');
+  });
+
+  it('should show name error if Validation fails', () => {
+    const validationError = faker.random.words();
+    const { sut } = makeSut({ validationError });
+    populateField(sut, 'name');
+    FormHelper.testStatusForField(sut, 'name', validationError);
   });
 });
