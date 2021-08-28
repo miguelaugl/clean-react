eb419d1357148e7de31524b2dfb0fed7d43b9a01
Miguel <miguelaugl@outlook.com>
2021-08-28 20:12:56 -0300

    test: ensure RemoteLoadSurveyList throws UnexpectedError on 403

--- src/data/test/mock-http.ts
+++ src/data/test/mock-http.ts
@@ -32,10 +32,14 @@ export class HttpPostClientSpy<R> implements HttpPostClient<R> {
   }
 }
 
-export class HttpGetClientSpy implements HttpGetClient {
+export class HttpGetClientSpy<R> implements HttpGetClient<R> {
   url: string;
+  response: HttpResponse<R> = {
+    statusCode: HttpStatusCode.SUCCESS,
+  };
 
-  async get(params: HttpGetParams): Promise<void> {
+  async get(params: HttpGetParams): Promise<HttpResponse<R>> {
     this.url = params.url;
+    return Promise.resolve(this.response);
   }
 }
--- src/data/usecases/load-survey-list/remote-load-survey-list.spec.ts
+++ src/data/usecases/load-survey-list/remote-load-survey-list.spec.ts
@@ -1,16 +1,19 @@
 import faker from 'faker';
 
+import { HttpStatusCode } from '@/data/protocols/http';
 import { HttpGetClientSpy } from '@/data/test';
+import { UnexpectedError } from '@/domain/errors';
+import { SurveyModel } from '@/domain/models';
 
 import { RemoteLoadSurveyList } from './remote-load-survey-list';
 
 type SutTypes = {
   sut: RemoteLoadSurveyList;
-  httpGetClientSpy: HttpGetClientSpy;
+  httpGetClientSpy: HttpGetClientSpy<SurveyModel[]>;
 };
 
 const makeSut = (url = faker.internet.url()): SutTypes => {
-  const httpGetClientSpy = new HttpGetClientSpy();
+  const httpGetClientSpy = new HttpGetClientSpy<SurveyModel[]>();
   const sut = new RemoteLoadSurveyList(url, httpGetClientSpy);
   return {
     sut,
@@ -24,5 +27,14 @@ describe('RemoteLoadSurveyList', () => {
     const { sut, httpGetClientSpy } = makeSut(url);
     await sut.loadAll();
     expect(httpGetClientSpy.url).toBe(url);
+  });
+
+  it('should throw UnexpectedError if HttpGetClientSpy returns 403', async () => {
+    const { sut, httpGetClientSpy } = makeSut();
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.FORBIDDEN,
+    };
+    const promise = sut.loadAll();
+    await expect(promise).rejects.toThrow(new UnexpectedError());
   });
 });
