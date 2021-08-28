a7e0d5d68cc4a761d918a425d56c949ac2409acc
Miguel <miguelaugl@outlook.com>
2021-08-28 20:51:04 -0300

    test: ensure AxiosHttpClient returns correct error on axios.get

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -68,5 +68,15 @@ describe('AxiosHttpClient', () => {
         body: axiosResponse.data,
       });
     });
+
+    it('should return correct error on axios.get', () => {
+      const { sut, mockedAxios } = makeSut();
+      mockedAxios.get.mockRejectedValueOnce({
+        response: mockHttpResponse(),
+      });
+      const promise = sut.get(mockGetRequest());
+      const promiseMockedResolveValue = mockedAxios.get.mock.results[0].value;
+      expect(promise).toEqual(promiseMockedResolveValue);
+    });
   });
 });
