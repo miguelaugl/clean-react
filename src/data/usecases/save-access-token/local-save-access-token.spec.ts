601e3c4ef9ae3c3e387df9af10b4a1388a3dd783
Miguel <miguelaugl@outlook.com>
2021-08-28 00:11:52 -0300

    test: ensure LocalSaveAccesssToken throws if no accessToken is provided

--- src/data/usecases/save-access-token/local-save-access-token.spec.ts
+++ src/data/usecases/save-access-token/local-save-access-token.spec.ts
@@ -1,6 +1,7 @@
 import faker from 'faker';
 
 import { SetStorageMock } from '@/data/test';
+import { UnexpectedError } from '@/domain/errors';
 
 import { LocalSaveAccessToken } from './local-save-access-token';
 
@@ -32,5 +33,11 @@ describe('LocalSaveAccessToken', () => {
     jest.spyOn(setStorageMock, 'set').mockRejectedValueOnce(new Error());
     const promise = sut.save(faker.datatype.uuid());
     await expect(promise).rejects.toThrow(new Error());
+  });
+
+  it('should throw if accessToken is falsy', async () => {
+    const { sut } = makeSut();
+    const promise = sut.save(undefined);
+    await expect(promise).rejects.toThrow(new UnexpectedError());
   });
 });
