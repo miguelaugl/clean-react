e2a4135239ddc264c8b10973f63fac21e73c7cd1
Miguel <miguelaugl@outlook.com>
2021-08-28 00:22:34 -0300

    test: ensure Login save accessToken if valid credentials are provided

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -74,4 +74,18 @@ describe('Login', () => {
     );
     cy.url().should('eq', `${baseUrl}/login`);
   });
+
+  it('should save accessToken if valid credentials are provided', () => {
+    cy.intercept(
+      { method: 'POST', url: /login/ },
+      { statusCode: 200, body: { accessToken: faker.datatype.uuid() } },
+    );
+    cy.getByTestId('email').focus().type('mango@gmail.com');
+    cy.getByTestId('password').focus().type('12345');
+    cy.getByTestId('submit').click();
+    cy.getByTestId('main-error').should('not.exist');
+    cy.getByTestId('spinner').should('not.exist');
+    cy.url().should('eq', `${baseUrl}/`);
+    cy.window().then((window) => assert.isOk(window.localStorage.getItem('accessToken')));
+  });
 });
