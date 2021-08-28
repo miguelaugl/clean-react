eb4b0f40dade9a537443dff8e5033f76767adad0
Miguel <miguelaugl@outlook.com>
2021-08-28 00:22:57 -0300

    test: ensure Login present UnexpectedError if invalid data is returned

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -88,4 +88,20 @@ describe('Login', () => {
     cy.url().should('eq', `${baseUrl}/`);
     cy.window().then((window) => assert.isOk(window.localStorage.getItem('accessToken')));
   });
+
+  it('should present UnexpectedError if invalid data is returned', () => {
+    cy.intercept(
+      { method: 'POST', url: /login/ },
+      { statusCode: 200, body: { invalidProperty: faker.datatype.uuid() } },
+    );
+    cy.getByTestId('email').focus().type('mango@gmail.com');
+    cy.getByTestId('password').focus().type('12345');
+    cy.getByTestId('submit').click();
+    cy.getByTestId('main-error').should(
+      'contain.text',
+      'Algo de errado aconteceu. Tente novamente em breve.',
+    );
+    cy.getByTestId('spinner').should('not.exist');
+    cy.url().should('eq', `${baseUrl}/login`);
+  });
 });
