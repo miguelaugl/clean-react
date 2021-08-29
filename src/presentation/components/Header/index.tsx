60738ee76ef03fb0701c0a58333eab022e60bcd2
Miguel <miguelaugl@outlook.com>
2021-08-29 16:47:49 -0300

    feat: present logged user name on Header component

--- src/presentation/components/Header/index.tsx
+++ src/presentation/components/Header/index.tsx
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import { Logo } from '@/presentation/components';
+import { ApiContext } from '@/presentation/contexts';
 
 import styles from './styles.scss';
 
 export const Header = () => {
+  const { getCurrentAccount } = useContext(ApiContext);
+
+  const account = getCurrentAccount();
+
   return (
     <header className={styles.headerWrap}>
       <div className={styles.headerContent}>
         <Logo />
         <div className={styles.logoutWrap}>
-          <span>Miguel</span>
+          <span>{account.name}</span>
           <a href='/'>Sair</a>
         </div>
       </div>
