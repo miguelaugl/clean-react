c75ff46a13ef2279527e0c2752ee1e5623e8ea79
Miguel <miguelaugl@outlook.com>
2021-08-30 20:58:27 -0300

    refactor: remove context application from Header

--- src/presentation/components/Header/index.tsx
+++ src/presentation/components/Header/index.tsx
@@ -1,21 +1,16 @@
-import React, { useContext } from 'react';
+import React from 'react';
 
 import { Logo } from '@/presentation/components';
-import { ApiContext } from '@/presentation/contexts';
 
 import styles from './styles.scss';
 
 export const Header = () => {
-  const { getCurrentAccount } = useContext(ApiContext);
-
-  const account = getCurrentAccount();
-
   return (
     <header className={styles.headerWrap}>
       <div className={styles.headerContent}>
         <Logo />
         <div className={styles.logoutWrap}>
-          <span>{account.name}</span>
+          <span>Miguel</span>
           <a href='/'>Sair</a>
         </div>
       </div>
