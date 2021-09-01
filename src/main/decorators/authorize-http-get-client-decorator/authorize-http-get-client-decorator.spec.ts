14a2284596a8ed2f9451893e14f476147e9114ed
Miguel <miguelaugl@outlook.com>
2021-09-01 15:56:22 -0300

    test: ensure AuthorizeHttpGetClientDecorator not add token is GetStorage is invalid

--- src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.spec.ts
+++ src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.spec.ts
@@ -1,24 +1,43 @@
-import { GetStorageSpy, mockGetRequest } from '@/data/test';
+import faker from 'faker';
+
+import { HttpGetParams } from '@/data/protocols/http';
+import { GetStorageSpy, HttpGetClientSpy, mockGetRequest } from '@/data/test';
 import { AuthorizeHttpGetClientDecotator } from '@/main/decorators';
 
 type SutTypes = {
   sut: AuthorizeHttpGetClientDecotator;
   getStorageSpy: GetStorageSpy;
+  httpGetClientSpy: HttpGetClientSpy;
 };
 
 const makeSut = (): SutTypes => {
   const getStorageSpy = new GetStorageSpy();
-  const sut = new AuthorizeHttpGetClientDecotator(getStorageSpy);
+  const httpGetClientSpy = new HttpGetClientSpy();
+  const sut = new AuthorizeHttpGetClientDecotator(getStorageSpy, httpGetClientSpy);
   return {
-    getStorageSpy,
     sut,
+    getStorageSpy,
+    httpGetClientSpy,
   };
 };
 
 describe('AuthorizeHttpGetClientDecotator', () => {
-  it('should call GetStorage with correct value', () => {
+  it('should call GetStorage with correct value', async () => {
     const { sut, getStorageSpy } = makeSut();
-    sut.get(mockGetRequest());
+    await sut.get(mockGetRequest());
     expect(getStorageSpy.key).toBe('account');
+  });
+
+  it('should not add headers if getStorage is invalid', async () => {
+    const { sut, httpGetClientSpy } = makeSut();
+    const httpRequest: HttpGetParams = {
+      url: faker.internet.url(),
+      headers: {
+        field: faker.database.column(),
+      },
+    };
+    await sut.get(httpRequest);
+    expect(httpGetClientSpy.url).toBe(httpRequest.url);
+    expect(httpGetClientSpy.headers).toEqual(httpRequest.headers);
   });
 });
