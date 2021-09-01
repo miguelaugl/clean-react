1fbda649c331af3f9dd3bf9a682a718033779939
Miguel <miguelaugl@outlook.com>
2021-09-01 14:20:49 -0300

    test: ensure SurveyList calls LoadSurveyList on reload click

--- src/presentation/pages/SurveyList/test.spec.tsx
+++ src/presentation/pages/SurveyList/test.spec.tsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import React from 'react';
 
 import { UnexpectedError } from '@/domain/errors';
@@ -60,5 +60,15 @@ describe('SurveyList Component', () => {
     await waitFor(() => screen.getByRole('heading'));
     expect(screen.queryByTestId('survey-list')).not.toBeInTheDocument();
     expect(screen.getByTestId('error')).toHaveTextContent(error.message);
+  });
+
+  it('should call LoadSurveyList on reload', async () => {
+    const loadSurveyListSpy = new LoadSurveyListSpy();
+    jest.spyOn(loadSurveyListSpy, 'loadAll').mockRejectedValueOnce(new UnexpectedError());
+    makeSut(loadSurveyListSpy);
+    await waitFor(() => screen.getByRole('heading'));
+    fireEvent.click(screen.getByTestId('reload'));
+    expect(loadSurveyListSpy.callsCount).toBe(1);
+    await waitFor(() => screen.getByRole('heading'));
   });
 });
