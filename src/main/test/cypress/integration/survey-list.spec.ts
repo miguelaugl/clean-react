53dcad79b05681e5b987d2a41a5347bb9313b444
Miguel <miguelaugl@outlook.com>
2021-09-02 20:15:42 -0300

    test: ensure SurveyList logout on link click

--- src/main/test/cypress/integration/survey-list.spec.ts
+++ src/main/test/cypress/integration/survey-list.spec.ts
@@ -29,4 +29,11 @@ describe('SurveyList', () => {
     const { name } = Helper.getLocalStorageItem('account');
     cy.getByTestId('username').should('have.text', name);
   });
+
+  it('should logout on logout link click', () => {
+    Http.mockUnexpectedError();
+    cy.visit('');
+    cy.getByTestId('logout').click();
+    Helper.testUrl('/login');
+  });
 });
