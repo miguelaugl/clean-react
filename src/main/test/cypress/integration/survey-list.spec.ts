96a4fb4d30729d3837dc0e33a7d1ca34273349dc
Miguel <miguelaugl@outlook.com>
2021-09-02 20:13:54 -0300

    test: ensure SurveyList logout on AccessDeniedError and present correct username

--- src/main/test/cypress/integration/survey-list.spec.ts
+++ src/main/test/cypress/integration/survey-list.spec.ts
@@ -16,4 +16,17 @@ describe('SurveyList', () => {
     cy.visit('');
     cy.getByTestId('error').should('have.text', 'Algo de errado aconteceu. Tente novamente em breve.');
   });
+
+  it('should logout on AccessDeniedError', () => {
+    Http.mockAccessDeniedError();
+    cy.visit('');
+    Helper.testUrl('/login');
+  });
+
+  it('should present correct username', () => {
+    Http.mockUnexpectedError();
+    cy.visit('');
+    const { name } = Helper.getLocalStorageItem('account');
+    cy.getByTestId('username').should('have.text', name);
+  });
 });
