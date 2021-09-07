71e9895c1b0a16c6dbaa6ea37de04f31b5b1995d
Miguel <miguelaugl@outlook.com>
2021-09-07 16:33:12 -0300

    test: ensure SurveyResult go to SurveyList on back button click

--- src/presentation/pages/SurveyResult/test.spec.tsx
+++ src/presentation/pages/SurveyResult/test.spec.tsx
@@ -16,7 +16,7 @@ type SutTypes = {
 };
 
 const makeSut = (loadSurveyResultSpy = new LoadSurveyResultSpy()): SutTypes => {
-  const history = createMemoryHistory();
+  const history = createMemoryHistory({ initialEntries: ['/', '/surveys/any_id'], initialIndex: 1 });
   const setCurrentAccountMock = jest.fn();
   render(
     <Router history={history}>
@@ -111,5 +111,12 @@ describe('SurveyResult Component', () => {
     fireEvent.click(screen.getByTestId('reload'));
     expect(loadSurveyResultSpy.callsCount).toBe(1);
     await waitFor(() => screen.getByTestId('survey-result'));
+  });
+
+  it('should go to SurveyList on back button click', async () => {
+    const { history } = makeSut();
+    await waitFor(() => screen.getByTestId('survey-result'));
+    fireEvent.click(screen.getByTestId('back-button'));
+    expect(history.location.pathname).toBe('/');
   });
 });
