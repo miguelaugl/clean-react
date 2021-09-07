6a843357d927802c318efc1fd4012a428c3e1faa
Miguel <miguelaugl@outlook.com>
2021-09-07 20:08:46 -0300

    feat: ensure RemoteSaveSurveyResult calls HttpClient with correct body

--- src/data/usecases/save-survey-result/remote-save-survey-result.ts
+++ src/data/usecases/save-survey-result/remote-save-survey-result.ts
@@ -9,6 +9,7 @@ export class RemoteSaveSurveyResult implements SaveSurveyResult {
     await this.httpClient.request({
       url: this.url,
       method: 'put',
+      body: params,
     });
     return null;
   }
