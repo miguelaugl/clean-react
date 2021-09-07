1a575e36f87131a317778f6cf9b64a519486224b
Miguel <miguelaugl@outlook.com>
2021-09-07 13:33:31 -0300

    test: ensure RemoteLoadSurveyResult returns SurveyResultModel on 200

--- src/data/usecases/load-survey-result/remote-load-survey-result.spec.ts
+++ src/data/usecases/load-survey-result/remote-load-survey-result.spec.ts
@@ -1,7 +1,7 @@
 import faker from 'faker';
 
 import { HttpStatusCode } from '@/data/protocols/http';
-import { HttpGetClientSpy } from '@/data/test';
+import { HttpGetClientSpy, mockRemoteSurveyResultModel } from '@/data/test';
 import { RemoteLoadSurveyResult } from '@/data/usecases';
 import { AccessDeniedError, UnexpectedError } from '@/domain/errors';
 
@@ -23,6 +23,10 @@ describe('RemoteLoadSurveyResult', () => {
   it('should call HttpGetClient with correct URL', async () => {
     const url = faker.internet.url();
     const { sut, httpGetClientSpy } = makeSut(url);
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.SUCCESS,
+      body: mockRemoteSurveyResultModel(),
+    };
     await sut.load();
     expect(httpGetClientSpy.url).toBe(url);
   });
@@ -52,5 +56,20 @@ describe('RemoteLoadSurveyResult', () => {
     };
     const promise = sut.load();
     await expect(promise).rejects.toThrow(new UnexpectedError());
+  });
+
+  it('should return a SurveyResult on 200', async () => {
+    const { sut, httpGetClientSpy } = makeSut();
+    const httpResult = mockRemoteSurveyResultModel();
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.SUCCESS,
+      body: httpResult,
+    };
+    const httpResponse = await sut.load();
+    expect(httpResponse).toEqual({
+      question: httpResult.question,
+      answers: httpResult.answers,
+      date: new Date(httpResult.date),
+    });
   });
 });
